f741754ff4ccfb3f19fc2667661e490bb646fac2
chenjiyang <893736346@qq.com>
2022-09-05 19:54:41 +0800

    9/5_Sequence

---
+++ Assets/SequenceFeedBack/Editor/MMFReadOnlyAttributeDrawer.cs
@@ -0,0 +1,63 @@
+﻿using UnityEditor;
+using UnityEngine;
+using  System.Reflection;
+namespace SequenceFeedBack
+{
+    /// <summary>
+    /// 完成只读特性的逻辑
+    /// </summary>
+    [CustomPropertyDrawer(typeof(MMFReadOnlyAttribute))]
+    public class MMFReadOnlyAttributeDrawer : PropertyDrawer
+    {
+        // Necessary since some properties tend to collapse smaller than their content
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property, label, true);
+        }
+
+        // Draw a disabled property field
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            GUI.enabled = false; // Disable fields
+            EditorGUI.PropertyField(position, property, label, true);
+            //意思是更新一下再禁用
+            GUI.enabled = true; // Enable fields
+        }
+    }
+   /// <summary>
+   /// 传入的字符创建按钮
+   /// </summary>
+    [CustomPropertyDrawer(typeof(MMFInspectorButtonAttribute))]
+    public class MMFInspectorButtonPropertyDrawer : PropertyDrawer
+    {
+        private MethodInfo _eventMethodInfo = null;
+
+        public override void OnGUI(Rect position, SerializedProperty prop, GUIContent label)
+        {
+            MMFInspectorButtonAttribute inspectorButtonAttribute = (MMFInspectorButtonAttribute)attribute;
+
+            float buttonLength = position.width;
+            Rect buttonRect = new Rect(position.x + (position.width - buttonLength) * 0.5f, position.y, buttonLength, position.height);
+
+            if (GUI.Button(buttonRect, inspectorButtonAttribute.MethodName))
+            {
+                System.Type eventOwnerType = prop.serializedObject.targetObject.GetType();
+                string eventName = inspectorButtonAttribute.MethodName;
+
+                if (_eventMethodInfo == null)
+                {
+                    _eventMethodInfo = eventOwnerType.GetMethod(eventName, BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+                }
+
+                if (_eventMethodInfo != null)
+                {
+                    _eventMethodInfo.Invoke(prop.serializedObject.targetObject, null);
+                }
+                else
+                {
+                    Debug.LogWarning(string.Format("InspectorButton: Unable to find method {0} in {1}", eventName, eventOwnerType));
+                }
+            }
+        }
+    }
+}
---
+++ Assets/SequenceFeedBack/Scripts/Attribute/FeedBackHelper.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace SequenceFeedBack
+{
+    /// <summary>
+    /// 只读属性，能看到值但不能修改
+    /// </summary>
+    public class MMFReadOnlyAttribute : PropertyAttribute { }
+    
+    
+    /// <summary>
+    /// 创建按钮（绑定方法名字）
+    /// </summary>
+    [System.AttributeUsage(System.AttributeTargets.Field)]
+    public class MMFInspectorButtonAttribute : PropertyAttribute
+    {
+        public readonly string MethodName;
+
+        public MMFInspectorButtonAttribute(string MethodName)
+        {
+            this.MethodName = MethodName;
+        }
+    }
+}
